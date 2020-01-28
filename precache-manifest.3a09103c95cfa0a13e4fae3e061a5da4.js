bd72e41b02c26e580470f33971dd72975162ba9b
Edward <35878962+eduard20028@users.noreply.github.com>
2020-01-28 02:52:27 +0200

    Updates

---
+++ precache-manifest.3a09103c95cfa0a13e4fae3e061a5da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ae4939c4171fc461dade31d868b5a2",
+    "url": "/movie-db-app/index.html"
+  },
+  {
+    "revision": "7d903db4562233eda4bf",
+    "url": "/movie-db-app/static/css/main.2df81997.chunk.css"
+  },
+  {
+    "revision": "729b773193507addedbb",
+    "url": "/movie-db-app/static/js/2.e792a5b8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movie-db-app/static/js/2.e792a5b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d903db4562233eda4bf",
+    "url": "/movie-db-app/static/js/main.8a90b813.chunk.js"
+  },
+  {
+    "revision": "d010319eb74c2df7cd15",
+    "url": "/movie-db-app/static/js/runtime-main.8338e1ec.js"
+  }
+]);
