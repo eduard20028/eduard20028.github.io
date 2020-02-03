8b13cdf5fa658fa6806370235435a819b1b66ce5
Edward <35878962+eduard20028@users.noreply.github.com>
2020-02-03 02:06:15 +0200

    Adding Hesh Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as ReactRouter,} from 'react-router-dom';
+import {HashRouter as ReactRouter} from 'react-router-dom';
 import MovieService from './services';
 import {MovieServiceProvider} from './components/movie-service-context';
 import ErrorBoundry from './components/error-boundry';
